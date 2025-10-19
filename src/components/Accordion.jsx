4f4956a6a6f93e1a7e925685400b52abdb56d1a5
Sergii <noodlex3m@gmail.com>
2025-10-19 18:15:15 +0300

    task 2

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Accordion() {
 	const faqData = [
 		{
@@ -19,13 +21,14 @@ function Accordion() {
 				"Це група для ФОП, які можуть мати дохід до 1167 МЗП на рік і сплачують 5% (без ПДВ) або 3% (з ПДВ).",
 		},
 	];
+	const [selectedId, setSelectedId] = useState(null);
 	return (
 		<div className="accordion">
 			<h2>Поширені питання (FAQ)</h2>
 			{faqData.map((item) => (
 				<div key={item.id} className="accordion-item">
-					<h3>{item.question}</h3>
-					<p>{item.answer}</p>
+					<h3 onClick={() => setSelectedId(item.id)}>{item.question}</h3>
+					{selectedId === item.id && <p>{item.answer}</p>}
 				</div>
 			))}
 		</div>
