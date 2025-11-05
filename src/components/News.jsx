37cd56e3764ec3dc4c96982c60b39cb25c6eca9c
Serhii Trishchuk <noodlex3m@hotmail.com>
2025-11-06 00:16:14 +0200

    add news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import newsData from "../data/newData";
+import "./News.css";
 
 function News() {
 	return (
-		<div>
+		<div className="news-container">
 			<h1>Останні Новини</h1>
-			<p>(Тут скоро з'являться статті...)</p>
+			<div className="news-list">
+				{newsData.map((article) => (
+					<div key={article.id} className="news-card">
+						<span className="news-category">{article.category}</span>
+						<h3>{article.title}</h3>
+						<p>{article.summary}</p>
+						<span className="news-date">{article.date}</span>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
