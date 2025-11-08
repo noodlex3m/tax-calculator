55e4a260228f01a6bab0e6f68e7417b5235f2a8b
Serhii Trishchuk <noodlex3m@hotmail.com>
2025-11-08 13:58:43 +0200

    pause Link news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import newsData from "../data/newData";
 import "./News.css";
+import { Link } from "react-router-dom";
 
 function News() {
 	return (
@@ -8,12 +9,14 @@ function News() {
 			<h1>Останні Новини</h1>
 			<div className="news-list">
 				{newsData.map((article) => (
-					<div key={article.id} className="news-card">
-						<span className="news-category">{article.category}</span>
-						<h3>{article.title}</h3>
-						<p>{article.summary}</p>
-						<span className="news-date">{article.date}</span>
-					</div>
+					<Link>
+						<div key={article.id} className="news-card">
+							<span className="news-category">{article.category}</span>
+							<h3>{article.title}</h3>
+							<p>{article.summary}</p>
+							<span className="news-date">{article.date}</span>
+						</div>
+					</Link>
 				))}
 			</div>
 		</div>
