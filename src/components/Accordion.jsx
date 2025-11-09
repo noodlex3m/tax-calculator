1760349e2ee1988d57706db17a919cd7a93d45d3
Serhii Trishchuk <noodlex3m@hotmail.com>
2025-11-09 13:48:27 +0200

    add filter search

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -5,10 +5,27 @@ import faqData from "../data/faqData";
 
 function Accordion() {
 	const [selectedId, setSelectedId] = useState(null);
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const filteredFaq = faqData.filter(
+		(item) =>
+			item.question.toLowerCase().includes(searchTerm.toLowerCase()) ||
+			item.shortAnswer.toLowerCase().includes(searchTerm.toLowerCase()) ||
+			item.fullAnswer.toLowerCase().includes(searchTerm.toLowerCase()) ||
+			item.category.toLowerCase().includes(searchTerm.toLowerCase())
+	);
+
 	return (
 		<div className="accordion">
 			<h2>Поширені питання (FAQ)</h2>
-			{faqData.map((item) => (
+			<input
+				type="text"
+				placeholder="Пошук за ключовими словами..."
+				className="search-bar"
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
+			/>
+			{filteredFaq.map((item) => (
 				<AccordionItem
 					key={item.id}
 					item={item}
