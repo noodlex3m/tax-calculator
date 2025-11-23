0aa5f6bbb2475a886da3395b11fd27b064dc64e5
Sergii <noodlex3m@gmail.com>
2025-11-23 12:17:00 +0200

    add burger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
+
+	const closeMenu = () => {
+		setIsMenuOpen(false);
+	};
+
 	return (
 		<header className="site-header">
 			<div className="header-container">
-				<Link to="/" className="logo">
+				<Link to="/" className="logo" onClick={closeMenu}>
 					Tax.Serh.One
 				</Link>
-				<nav className="main-nav">
-					<NavLink to="/">Головна</NavLink>
-					<NavLink to="/faq">Поширені Питання (FAQ)</NavLink>
-					<NavLink to="/calculator">Калькулятор</NavLink>
-					<NavLink to="/news">Новини</NavLink>
+				<div
+					className={`burger-icon ${isMenuOpen ? "open" : ""}`}
+					onClick={toggleMenu}
+				>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+
+				<nav className={`main-nav ${isMenuOpen ? "active" : ""}`}>
+					<NavLink to="/" onClick={closeMenu}>
+						Головна
+					</NavLink>
+					<NavLink to="/faq" onClick={closeMenu}>
+						Запитання
+					</NavLink>
+					<NavLink to="/calculator" onClick={closeMenu}>
+						Калькулятор
+					</NavLink>
+					<NavLink to="/news" onClick={closeMenu}>
+						Новини
+					</NavLink>
 				</nav>
 			</div>
 		</header>
