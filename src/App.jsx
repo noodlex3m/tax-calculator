c9df2699c6782410d84be07559f38403d34b6879
Serhii Trishchuk <noodlex3m@hotmail.com>
2025-12-28 14:54:08 +0200

    add Suspense loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import { Routes, Route } from "react-router-dom";
-import Faq from "./components/Faq";
 import "./App.css";
-import TaxCalculator from "./components/TaxCalculator";
-import News from "./components/News";
-import ArticlePage from "./components/ArticlePage";
+import Home from "./components/Home";
+
+import { lazy, Suspense } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./components/Home";
-import NotFound from "./components/NotFound";
+
+const TaxCalculator = lazy(() => import("./components/TaxCalculator"));
+const News = lazy(() => import("./components/News"));
+const Faq = lazy(() => import("./components/Faq"));
+const ArticlePage = lazy(() => import("./components/ArticlePage"));
+const NotFound = lazy(() => import("./components/NotFound"));
 
 function App() {
 	return (
 		<>
 			<Header />
 			<main className="main-content">
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/faq" element={<Faq />} />
-					<Route path="/calculator" element={<TaxCalculator />} />
-					<Route path="/news/:id" element={<ArticlePage />} />
-					<Route path="/news" element={<News />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
+				<Suspense fallback={<div className="loading">Завантаження...</div>}>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/faq" element={<Faq />} />
+						<Route path="/calculator" element={<TaxCalculator />} />
+						<Route path="/news/:id" element={<ArticlePage />} />
+						<Route path="/news" element={<News />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Suspense>
 			</main>
 			<Footer />
 		</>
