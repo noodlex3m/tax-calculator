e09755a135c21f0180b99081cdc6cde5a62cbf7f
Serhii Trishchuk <noodlex3m@hotmail.com>
2026-01-01 23:07:57 +0200

    feat: add PWA support with manifest and icons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: true,
+			},
+			manifest: {
+				name: "Калькулятор Податків ФОП 2026",
+				short_name: "Tax FOP",
+				description: "Зручний калькулятор податків для ФОП",
+				theme_color: "#ffffff",
+				background_color: "#ffffff",
+				display: "standalone",
+				icons: [
+					{
+						src: "/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+});
